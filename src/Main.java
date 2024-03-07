e87ba402a3643ea888dca2965c082888fb4c10c6
HMTaskTI <henrique.macaneiro@taskti.com.br>
2024-03-06 22:10:09 -0300

    Popula classes na main e chama metodo mostraTodosOsDados

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tarefa album");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o nome do compositor:");
+        String nomeCompositor = scanner.nextLine();
+        System.out.println("Digite a idade do compositor:");
+        int idadeCompositor = scanner.nextInt();
+        Pessoa compositor = new Pessoa(nomeCompositor, idadeCompositor);
+
+
+        System.out.println("Digite o gênero do álbum:");
+        String generoAlbum = scanner.nextLine();
+        System.out.println("Digite o ano do álbum:");
+        int anoAlbum = scanner.nextInt();
+        System.out.println("Digite o nome do álbum:");
+        scanner.nextLine();
+        String nomeAlbum = scanner.nextLine();
+        System.out.println("Digite o nome do artista:");
+        String artistaAlbum = scanner.nextLine();
+
+        System.out.println("Digite o número de músicas no álbum:");
+        int numMusicas = scanner.nextInt();
+        ArrayList<Musica> musicasAlbum = new ArrayList<Musica>();
+        for (int i = 0; i < numMusicas; i++) {
+            System.out.println("Digite o título da música:");
+            scanner.nextLine();
+            String tituloMusica = scanner.nextLine();
+            System.out.println("Digite a duração da música:");
+            String duracaoMusica = scanner.nextLine();
+            Musica musica = new Musica(tituloMusica, duracaoMusica, compositor);
+            musicasAlbum.add(new Musica(tituloMusica, duracaoMusica, compositor));
+        }
+
+        Album album = new Album(generoAlbum, anoAlbum, nomeAlbum, artistaAlbum);
+        album.setMusicas(musicasAlbum);
+        album.mostraTodosOsDados();
     }
 }
