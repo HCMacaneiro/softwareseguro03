25efacfd8cc73e20ac4dc63a17016c2b45ceaee2
HMTaskTI <henrique.macaneiro@taskti.com.br>
2024-03-06 21:45:51 -0300

    Cria atributos e metodos classe Musica

--- src/Musica.java
+++ src/Musica.java
@@ -1,2 +1,28 @@
 public class Musica {
+    private String titulo;
+    private String duracao;
+    private Pessoa compositor;
+
+    public Musica(String titulo, String duracao, Pessoa compositor) {
+        this.titulo = titulo;
+        this.duracao = duracao;
+        this.compositor = compositor;
+    }
+
+    public void TocarMusica() {
+        System.out.println("Tocando " + this.titulo + " de " + this.compositor + " com duração " + this.duracao);
+    }
+
+
+    public Pessoa getCompositor() {
+        return compositor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
 }
